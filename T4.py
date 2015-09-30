0c26a1c054c116f76eb86a757442f55573be8583
user <Laaia@hotmail.com>
2015-09-30 00:11:39 -0300

    T4

--- T4.py
+++ T4.py
@@ -3,6 +3,19 @@
 
 from sets import Set
 from operator import itemgetter
+import itertools
+
+
+try:
+    dic_arq = open("dic.txt", "rb")
+except IOError:
+    print "Erro na abertura dos arquivos"
+
+dicio = dic_arq.readlines()
+dic = Set([])
+for i in dicio:
+    dic.add(i.replace("\n", ""))
+
 
 # Calcula a quantidade de ocorrências de uma string de
 # tamanho n em um arquivo de texto
@@ -67,13 +80,13 @@ def padroes(line, modo):
     d = d.replace("\n", " ")
     d = d.replace("\r", " ")
 
-    dic = Set(d.split())
+    di = Set(d.split())
     #print dic
 
     #print "\n\nPadrões: "
     string = ''
     dicPadroes = {}
-    for palavra in dic:
+    for palavra in di:
     	p = padrao(palavra)
         string += p + "\n"
         dicPadroes[p] = string.count(p+"\n")
@@ -89,6 +102,19 @@ def padroes(line, modo):
     s.write(strg)
     s.close()
 
+def searchWords(text):
+    hit = 0.0
+    lines = 0.0
+    text = text.split()
+    text = Set(text)
+    for word in text:
+        if (word in dic) or (word.lower() in dic):
+            hit += 1.0
+    #print "achei word = " + word
+
+    lines += 1.0
+    print (hit/lines)*100
+    return (hit/lines)*100
 # - - - M A I N - - - #
 
 # Geração de padrões, triplas, duplas e letras com base num dicionario
@@ -114,7 +140,8 @@ try:
     tCif = open("3_cif.txt", "r+")
 
     cif = open("cif.txt", "r+")
-    s = open("saida.txt", "ab")
+
+
 except IOError:
     print "Erro na abertura dos arquivos"
 
@@ -122,16 +149,12 @@ except IOError:
 letrasDic = lDic.read().split()
 
 duplasDic = dDic.read().split()
-#duplasDic = duplasDic[::-1].split()
 
 triplasDic = tDic.read().split()
-#triplasDic = triplasDic[::-1].split()
 
 letrasCif = lCif.read().split()
-#letrasCif = letrasCif[::-1].split()
 
 duplasCif = dCif.read().split()
-#letrasCif = duplasCif[::-1].split()
 
 triplasCif = tCif.read().split()
 
@@ -140,32 +163,51 @@ hash = {}
 # Associação entre os dados do dicionario com o texto cifrado
 # Construção da chave
 
-for i in xrange(len(triplasDic)-1, 0, -2):
-	#print triplasDic[i]+" "+ triplasCif[i]
-	for (x, y) in zip(triplasDic[i], triplasCif[i]):
-		hash[x] = y
-		#hash[x].append(y)
+#hash[' '] = letrasCif[len(letrasCif) - 1]
 
-for i in xrange(len(duplasDic)-2, 0, -2):
-	#print duplasDic[i]+" "+ duplasCif[i]
-	for (x, y) in zip(duplasDic[i], duplasCif[i]):
-		if x not in hash:
-			hash[x] = y
+add = ''
+for i in xrange(1, 10, 2):
+    #print triplasDic[i]+" "+ triplasCif[i]
+    for (x, y) in zip(triplasCif[len(triplasCif)-i], triplasDic[len(triplasDic)-i]):
+        #print "x = "+x+" y = "+y
+        if y not in add:
+            hash[x] =[]
+            hash[x].append(y)
+            add += y
 
-"""for i in xrange(1, len(letrasDic), 2):
-	x, y = letrasDic[len(letrasDic) - (i+1)], letrasCif[len(letrasCif) - i]
-	#print x+" "+y
-	if x not in hash:
-		hash[x] = y
-"""
+for i in xrange(1, 10, 2):
+    #print duplasDic[i]+" "+ duplasCif[i]
+    for (x, y) in zip(triplasCif[len(triplasCif)-i], triplasDic[len(triplasDic)-i]):
+        if y not in add:
+            hash[x] = []
+            hash[x].append(y)
+
+for i in xrange(1, 10, 2):
+    x, y = letrasCif[len(letrasCif) - i], letrasDic[len(letrasDic) - (i+1)]
+    #print x+" "+y
+    if y not in add:
+        hash[x] = []
+        hash[x].append(y)
+
 entrada = cif.read()
-print hash
-
-# Quebra do texto cifrado
-for i in hash:
-	#print hash[i]+" "+ i
-	entrada = entrada.replace(hash[i], i)
-
-s.write(entrada)
-
-#quanto de cada pegar: I'm god!
+hash[letrasCif[len(letrasCif)-1]].append(" ")
+for h in hash:
+    print hash[h]
+itr = 0
+key = ''
+for x in hash:
+    lista = hash[x]
+    try:
+        key += lista[itr]
+    except IndexError:
+        pass
+for a in itertools.permutations(key, len(key)):
+    l = 0
+    # Quebra do texto cifrado
+    for i in hash:
+    	entrada = entrada.replace(i, key[l])
+        l += 1
+    if searchWords(entrada) >= 50:
+        print "Chave = " + key
+        s.write(entrada)
+        break
