07e3aa3ee6aaadec1a40cad52f086d0f8c5d8ab9
Laaia <Laaia@hotmail.com>
2015-10-06 22:30:34 -0300

    T4

--- T4.py
+++ T4.py
@@ -16,6 +16,7 @@ dic = Set([])
 for i in dicio:
     dic.add(i.replace("\n", ""))
 
+dicP = {} #Dicionário de padrões que aparecem somente uma vez
 
 # Calcula a quantidade de ocorrências de uma string de
 # tamanho n em um arquivo de texto
@@ -40,7 +41,7 @@ def frequencia(line, modo):
         i += 1
         f += 1
 
-    janela = sorted(janela.items(), key=itemgetter(0))
+    janela = sorted(janela.items(), key=itemgetter(0), reverse=True)
 
     strg = ''
     for i in janela:
@@ -70,7 +71,7 @@ def padrao(palavra):
 
 def padroes(line, modo):
     try:
-        s = open("padrao_"+modo+".txt", "ab")
+        s = open("padrao_"+modo+".txt", "wb")
         dicionario = open(line, "r+")
     except IOError:
         print "Erro na abertura dos arquivos"
@@ -85,22 +86,19 @@ def padroes(line, modo):
 
     #print "\n\nPadrões: "
     string = ''
-    dicPadroes = {}
     for palavra in di:
     	p = padrao(palavra)
+        #print string.count(p)
+        dicP[p] = palavra
         string += p + "\n"
-        dicPadroes[p] = string.count(p+"\n")
 
-    #print string
-    dicPadroes = sorted(dicPadroes.items(), key=itemgetter(1), reverse=true)
+    for p in string.split():
+        if string.count(p) != 1:
+            try:
+                del dicP[p]
+            except KeyError:
+                pass
 
-    strg = ''
-    for i in dicPadroes:
-    	a, b = i
-        strg = strg + str(a) + " " + str(b) + "\n"
-    print strg
-    s.write(strg)
-    s.close()
 
 def searchWords(text):
 	hit = 0.0
@@ -118,115 +116,101 @@ def searchWords(text):
 
 # Geração de padrões, triplas, duplas e letras com base num dicionario
 """frequencia("1", "dic")
-frequencia("2", "dic")"""
-frequencia("3", "dic")
-#padroes("dic.txt","dic")
+frequencia("2", "dic")
+frequencia("3", "dic")"""
+padroes("dic.txt","dic")
 
 # Geração de padrões, triplas, duplas e letras com base no texto cifrado
-frequencia("1", "cif")
-"""frequencia("2", "cif")
+"""frequencia("1", "cif")
+frequencia("2", "cif")
 frequencia("3", "cif")"""
-#padroes("cif.txt","cifra")
 
 
 try:
-    lDic = open("1_dic.txt", "r+")
-    dDic = open("2_dic.txt", "r+")
-    tDic = open("3_dic.txt", "r+")
+    lDic = open("1_dic.txt", "rb")
+    dDic = open("2_dic.txt", "rb")
+    tDic = open("3_dic.txt", "rb")
 
-    lCif = open("1_cif.txt", "r+")
-    dCif = open("2_cif.txt", "r+")
-    tCif = open("3_cif.txt", "r+")
+    lCif = open("1_cif.txt", "rb")
+    dCif = open("2_cif.txt", "rb")
+    tCif = open("3_cif.txt", "rb")
 
-    cif = open("cif.txt", "r+")
+    cif = open("cif.txt", "rb")
     s = open("saida.txt", "wb")
+    tex = open("dic.txt", "rb")
 except IOError:
     print "Erro na abertura dos arquivos"
 
 
-letrasDic = lDic.read().split()
+letrasDic = lDic.read()
 
-duplasDic = dDic.read().split()
+duplasDic = dDic.read()
 
-triplasDic = tDic.read().split()
+triplasDic = tDic.read()
 
-letrasCif = lCif.read().split()
+letrasCif = lCif.read()
 
-duplasCif = dCif.read().split()
+duplasCif = dCif.read()
 
-triplasCif = tCif.read().split()
+triplasCif = tCif.read()
 
 
 hash = {}
 # Associação entre os dados do dicionario com o texto cifrado
 # Construção da chave
 
-#hash[' '] = letrasCif[len(letrasCif) - 1]
-hash[letrasCif[len(letrasCif)-1]] = []
-hash[letrasCif[len(letrasCif)-1]].append(" ")
+entrada = cif.read()
+texto = tex.read()
+
+i = 0
+for p in dicP:
+    if len(p) > 10:
+        i+=1
+        palavra = dicP[p]
+        for x in range(0, len(entrada)-len(p)):
+            if padrao(entrada[x:x+len(p)]) == p:
+                y = 0
+                print entrada[x:x+len(p)]
+                print p
+                print palavra
+                for a in range(x, x+len(p)):
+                    hash[entrada[a]] = palavra[y]
+                    y += 1
+                break
+        if i == 10:
+            break
+
 add = ''
 
-"""for i in xrange(2, 9):
-    #print triplasDic[i]+" "+ triplasCif[i]
-    for (x, y) in zip(triplasCif[len(triplasCif)-i], triplasDic[len(triplasDic)-i]):
-        try:
-        	hash[x].append(y)
-        except Exception, e:
-			hash[x] =[]
-			hash[x].append(y)
-        add += y
+for x in range(0, 12):
+    if duplasDic[x] not in add:
+        hash[triplasCif[x]] = triplasDic[x]
+        add += triplasDic[x]
 
-for i in xrange(1, 20, 2):
-    #print duplasDic[i]+" "+ duplasCif[i]
-    for (x, y) in zip(triplasCif[len(triplasCif)-i], triplasDic[len(triplasDic)-i]):
-        #if y not in add:
-		try:
-			hash[x].append(y)
-		except Exception, e:
-			hash[x] =[]
-			hash[x].append(y)
+for x in range(0, 6):
+    if duplasDic[x] not in add:
+        hash[duplasCif[x]] = duplasDic[x]
+        add += duplasDic[x]
 
-for i in xrange(1, len(letrasCif), 2):
-	x, y = letrasCif[len(letrasCif) - i], letrasDic[len(letrasDic) - (i+1)]
-	#print x+" "+y
-	#if y not in add:
-	try:
-		hash[x].append(y)
-	except Exception, e:
-		hash[x] =[]
-		hash[x].append(y)"""
+for x in range(0, 5):
+    if letrasDic[x] not in add:
+        hash[letrasCif[x]] = letrasDic[x]
+        add += letrasDic[x]
 
-entrada = cif.read()
+print hash
+print "--"*8
 
-for h in hash:
-    print hash[h]
-itr = 0
+#hash[entrada[68]] = texto[68]
 
-while itr < len(hash):
-	key = ''
-	for x in hash:
-		lista = hash[x]
-		try:
-			key += lista[itr]
-		except IndexError:
-			pass
-	tentativa = 0
-	for a in itertools.permutations(key, len(key)):
-		l = 0
-		print a
-		# Quebra do texto cifrado
-		for i in hash:
-			try:
-				entrada = entrada.replace(i, key[l])
-			except IndexError:
-				pass
-			l += 1
-		if searchWords(entrada) >= 0.1 and tentativa > 	45:
-			print "Chave = " + key
-			s.write(entrada)
-			itr = len(hash)
-			break
-		if tentativa > 50:
-			break
-		tentativa+=1
-	itr += 1
+saida = ''
+for i in entrada:
+    try:
+        saida += hash[i]
+    except KeyError:
+        saida += '_'
+
+
+
+searchWords(saida)
+
+s.write(saida)
